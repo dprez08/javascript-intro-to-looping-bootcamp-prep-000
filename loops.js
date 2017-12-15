35601f6de73059d37e5a222640bc2b8859c1fdaf
David Perez <davperez89@gmail.com>
2017-12-15 00:28:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,22 +20,11 @@ function whileLoop(n) {
 
 function doWhileLoop(array) {
   function maybetrue(){
-    return Math.random() >= 0.5
+    return Math.random() >= 0.5;
   }
   do {
-    array.pop();{
-    while (array.length >1 && maybeTrue());
+    array.pop();
+  } while (array.length >1 && maybeTrue());
     return array;
 }
 
-
-+function doWhileLoop(array) {
- +  function maybeTrue() {
- +    return Math.random() >= 0.5
- +  }
- +
- +  do {
- +    array.pop();
- +  } while (array.length > -1 && maybeTrue());
- +    return array;
- +}]
